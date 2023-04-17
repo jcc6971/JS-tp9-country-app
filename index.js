3f9eef72d1051d857e493cf11f28ebb499063eb1
jcc <jccollart@outlook.be>
2023-04-18 00:18:16 +0200

    tri-croietdecroi-ok

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 // 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
 let country = [];
 let recherche = [];
-let search = [];
+
+let minto;
+let maxto;
 const cContainer = document.querySelector(".countries-container");
 
 const importCountry = async () => {
@@ -10,7 +12,7 @@ const importCountry = async () => {
     .then((res) => res.json())
     .then((donnee) => (country = donnee));
 };
-
+diplayCountries();
 // -------entrée nom--------
 inputSearch.addEventListener("input", (e) => {
   e.preventDefault();
@@ -25,24 +27,41 @@ inputRange.addEventListener("input", (e) => {
 
   diplayCountries();
 });
+// -------tri croissant-------
+minToMax.addEventListener("click", () => {
+  minto = true;
+  // country.sort((a, b) => a.population - b.population);
+  diplayCountries();
+});
+// -----tri decroissant----
+maxToMin.addEventListener("click", () => {
+  maxto = true;
+  diplayCountries();
+});
 
 async function diplayCountries() {
   await importCountry();
 
   country.length = rangeValue.textContent;
+  console.log(minto);
+  if (minto === true) country.sort((a, b) => a.population - b.population);
+  else if (maxto === true) country.sort((a, b) => b.population - a.population);
+
   cContainer.innerHTML = country
     .filter((count) => count.name.common.includes(recherche))
-    .map(
-      (affiche) => `
-    <div class="card">
-    <img src=${affiche.flags.png}>
-    <h2> ${affiche.name.common}</h2>
-    <h3> ${affiche.capital}
-    <p> Population: ${affiche.population} </p>
-    </div>
-    `
-    )
+    .map((affiche) => {
+      return `
+        <div class="card">
+        <img src=${affiche.flags.png}>
+        <h2> ${affiche.name.common}</h2>
+        <h3> ${affiche.capital}
+        <p> Population: ${affiche.population} </p>
+        </div>
+        `;
+    })
     .join("");
+  minto = false;
+  maxto = false;
 }
 
 // // // 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
