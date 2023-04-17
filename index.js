0a1c8d714101bd7c73e17c96a73b3fe7cc41af03
jcc <jccollart@outlook.be>
2023-04-17 16:39:06 +0200

    cursor-rechercheok

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 // 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
+// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
+let country = [];
+let recherche = [];
+let search = [];
+const cContainer = document.querySelector(".countries-container");
 
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
+const importCountry = async () => {
+  await fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((donnee) => (country = donnee));
+};
 
-// 3 - Passer les données à une variable
+// -------entrée nom--------
+inputSearch.addEventListener("input", (e) => {
+  e.preventDefault();
+  recherche = e.target.value;
+  diplayCountries();
+  console.log(recherche);
+});
+// // -------curseur------
+inputRange.addEventListener("input", (e) => {
+  e.preventDefault();
+  rangeValue.textContent = e.target.value;
 
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
+  diplayCountries();
+});
 
-// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-coutry.name.includes(inputSearch.value);
+async function diplayCountries() {
+  await importCountry();
 
-// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
+  country.length = rangeValue.textContent;
+  cContainer.innerHTML = country
+    .filter((count) => count.name.common.includes(recherche))
+    .map(
+      (affiche) => `
+    <div class="card">
+    <img src=${affiche.flags.png}>
+    <h2> ${affiche.name.common}</h2>
+    <h3> ${affiche.capital}
+    <p> Population: ${affiche.population} </p>
+    </div>
+    `
+    )
+    .join("");
+}
 
-// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
+// // // 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
+
+// // // 3 - Passer les données à une variable
+
+// // // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
+
+// // // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
+// // // coutry.name.includes(inputSearch.value);
+
+// // // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
+
+// // // 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
